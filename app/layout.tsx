4fee5471077fd817a59748c32f426c1f295abac0
Devin Lee <lee.devinlee14@gmail.com>
2026-09-06 11:02:30 +0700

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
+import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
-  title: 'Devin Lee - About me',
-  description: 'Senior Data Analyst specializing in data analytics, automation, business intelligence, and data visualization.',
-}
+export const metadata: Metadata = {
+  title: "Devin Lee | Portfolio",
+  description:
+    "Senior Data Analyst specializing in data analytics, automation, business intelligence, and data visualization.",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
